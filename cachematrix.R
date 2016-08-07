cd598d0d031591c84d230771311bd16479c6a1bc
benarobe <benjamin.roberts7@4JLT.com>
2016-08-06 21:27:32 -0400

    Added relevant code and comments.
    Coding completed per instructions in coursera course. It was easier to
    modify the example "cacheMean" functions than it was to figure out what
    was going on. I think I have it though.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions work together to create a special object containing 
+## a matrix as well as the matrix's inverse so that after the inverse
+## is originally calculated once, the result will be available if needed
+## again.
 
-## Write a short comment describing this function
+## This function creates the matrix object along with some functions
+## for getting and setting matrix values and inverse. The function returns
+## these functions in a list.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse of the matrix. It returns the stored
+## inverse value if available, and calculates the inverse if it had
+## not been stored.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
+  
 }
+
